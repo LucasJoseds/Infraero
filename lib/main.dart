8e1d4e4320060d5829dd594bf59945e7fe8bce5a
talesmm14 <talesmelquiades@hotmail.com>
2022-03-24 09:48:03 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Teste'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Página inicial'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
+  class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+  appBar: AppBar(
+  title: const Text('Second Route'),
+  ),
+  body: Center(
+  child: ElevatedButton(
+  onPressed: () {
+  Navigator.pop(context);
+  },
+  child: const Text('Voltar para página incial!'),
+  ),
+  ),
+  );
+  }
+  }
+
+
+
+
+
+
